b643c05682faff7bc3f6219db18fa96b99817038
kevlarr <6306496+kevlarr@users.noreply.github.com>
2021-08-25 11:08:11 -0400

    api call to populate html

--- web/server.py
+++ web/server.py
@@ -1,7 +1,40 @@
+from datetime import datetime
+from os import environ
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import requests
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-  return "Hello, world!"
+    api_url = environ.get("API_URL")
+    resp = requests.get(api_url).json()
+
+    dt = datetime.fromisoformat(resp["updated_at"])
+    date = dt.strftime("%Y-%m-%d")
+    time = dt.strftime("%I:%M:%S %p")
+
+    return f"""
+    <html>
+        <head>
+            <style>
+                html, body {{
+                    height: 100%;
+                }}
+
+                body {{
+                    align-items: center;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                }}
+            </style>
+        </head>
+        <body>
+            <h1>{date}</h1>
+            <p>{time}</p>
+        </body>
+    </html>
+    """
